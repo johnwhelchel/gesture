f6b3c13504c4653c5bb3084984f984c7ac82c4e4
johnwhelchel <john.l.whelchel@gmail.com>
2016-01-30 03:07:14 -0500

    Adding some fake training and testing

--- ml_demo.py
+++ ml_demo.py
@@ -10,8 +10,8 @@ from sklearn.multiclass import OneVsRestClassifier
 
 WORDS = ["father", "sorry"]
 BASE_DIR = os.getcwd()
-TRAINING_DIR = os.path.join(os.path.join(BASE_DIR, ".."), "training")
-TESTING_DIR = os.path.join(os.path.join(BASE_DIR, ".."), "testing")
+TRAINING_DIR = os.path.join(BASE_DIR, "training")
+TESTING_DIR = os.path.join(BASE_DIR, "testing")
 VERBOSE = True
 
 def V(text):
@@ -22,25 +22,32 @@ X_train = []
 y_train = []
 for word in WORDS:
     V("Reading in training data for word " + word)
+    expectedWordEnumVal = WORDS.index(word)
     count = 0
-    filename = word + count
-    filelocation = os.path.join(TRAINING_DIR, filename)
-    filep = pathlib.Path(filelocation)
-    if filep.exists():
-        V("Read file " + str(count))
-        y_train.push([1 if WORDS[x] == word else 0 for x in range(len(WORDS)))
-        with open(filelocation, mode='r') as f:
-            X_train.push(f.read())
-        count += 1
-    else:
-        V("No more files found for word " + word)
+    fileExists = True
+    while(fileExists):    
+        filename = word + str(count)
+        filelocation = os.path.join(TRAINING_DIR, filename)
+        filep = pathlib.Path(filelocation)
+        if filep.exists():
+            V("Read file " + str(count))
+            #y_train.append([1 if WORDS[x] == word else 0 for x in range(len(WORDS))) multilabel
+            y_train.append(expectedWordEnumVal)
+            with open(filelocation, mode='r') as f:
+                X_train.append(f.read())
+            count += 1
+        else:
+            V("No more files found for word " + word)
+            fileExists = False
 
 X_train_numpy = np.array(X_train)
 
 X_test = []
+y_test_expected = []
 for filename in os.listdir(TESTING_DIR):
-    with open(filename, mode='r') as f:
-        X_test.push(f.read())
+    with open(os.path.join(TESTING_DIR, filename), mode='r') as f:
+        X_test.append(f.read())
+        y_test_expected.append(WORDS.index("".join([i for i in filename if not i.isdigit()])))
 
 X_test_numpy = np.array(X_test)
 
@@ -50,5 +57,5 @@ classifier = Pipeline([
     ('clf', OneVsRestClassifier(LinearSVC()))])
 classifier.fit(X_train_numpy, y_train)
 predicted = classifier.predict(X_test_numpy)
-for item, labels in zip(X_test, predicted):
-    print(str(item) + ' => ' + ', '.join(WORDS[ind] for ind, x in enumerate(labels) if x == 1))
+for idx, (item, classification) in enumerate(zip(y_test_expected, predicted)):
+    print(str(WORDS[item]) + ' (actual) ' + X_test[idx] + ' ' + str(WORDS[classification]) + ' (predicted)')
